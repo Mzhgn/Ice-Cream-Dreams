0d5a2873a9a65af14e528ddf39bd4430faf8bb81
Mozhgan <mojganshah@gmail.com>
2024-08-17 15:02:54 +0330

    photos modified

---
+++ js/store.js
@@ -0,0 +1,50 @@
+let allProducts = [
+  {
+    id: 1,
+    title: "icecream Caramel",
+    price: 14.2,
+    img: "images/caramel-ice.png",
+  },
+  {
+    id: 2,
+    title: "icecream Banana",
+    price: 17.89,
+    img: "images/Banana.png",
+  },
+  {
+    id: 3,
+    title: "icecream blue berry",
+    price: 10.28,
+    img: "images/blueberry.png",
+  },
+  {
+    id: 4,
+    title: "icecream chocolate",
+    price: 9.58,
+    img: "images/chocolate.png",
+  },
+  {
+    id: 5,
+    title: "icecream coconut",
+    price: 11.27,
+    img: "images/coconut.png",
+  },
+  {
+    id: 6,
+    title: "icecream Strawberry",
+    price: 12.36,
+    img: "images/strawberry.png",
+  },
+  {
+    id: 7,
+    title: "icecream oreo",
+    price: 11.25,
+    img: "images/oreo.png",
+  },
+  {
+    id: 1,
+    title: "icecream vanilla",
+    price: 10.5,
+    img: "images/vanilla.png",
+  },
+];
